b737073f4e7bc96793a45198c02ee4f5915dd0c1
Prosenjeet Shil <110659610+prosenjeetshil@users.noreply.github.com>
2024-12-19 07:43:37 +0530

    router prac

---
+++ routerprac/components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px", backgroundColor: "#f0f0f0", borderRadius: "10px" }}>
+      <h1 style={{ color: "#333" }}>{"Welcome to the Contact Page"}</h1>
+      <p style={{ color: "#666", fontSize: "1.2em" }}>This is the home page of our website. Feel free to explore around!</p>
+    </div>
+  );
+};
+export default Contact;
---
+++ routerprac/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+
+  return (
+    <div style={{ textAlign: "center", padding: "20px", backgroundColor: "#f0f0f0", borderRadius: "10px" }}>
+      <h1 style={{ color: "#333" }}>{"Welcome to the Home Page"}</h1>
+      <p style={{ color: "#666", fontSize: "1.2em" }}>This is the home page of our website. Feel free to explore around!</p>
+    </div>
+  );
+};
+
+export default Home;
